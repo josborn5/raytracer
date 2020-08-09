9406e3ee497a2648a6fc4162982b22cb14303d3a
John Osborn <john.osborn@relativity.com>
2020-08-08 21:18:02 -0500

    Adding gamma correction to the image color

--- code/color.h
+++ code/color.h
@@ -10,11 +10,11 @@ void write_color(std::ostream &out, const vec3 &color, int samples_per_pixel)
 	float g = color.y();
 	float b = color.z();
 
-	// Divide the color by the number of samples
+	// Divide the color by the number of samples and gamma-correct got gamma=2.0
 	float scale = 1.0f / samples_per_pixel;
-	r *= scale;
-	g *= scale;
-	b *= scale;
+	r = sqrt(scale * r);
+	g = sqrt(scale * g);
+	b = sqrt(scale * b);
 
 	const float char_conversion = 256.0f;
 	const float min_color = 0.0f;
