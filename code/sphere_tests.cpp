e432c0548f11bb055ade7ab73f0839b40a8b0a54
John Osborn <john.osborn@relativity.com>
2020-08-08 20:55:18 -0500

    sphere unit tests

---
+++ code/sphere_tests.cpp
@@ -0,0 +1,80 @@
+#include "sphere.h"
+#include <assert.h>
+#include <iostream>
+
+void print_hit_result(const hit_record &hit, bool collision)
+{
+	std::cout << "collision: " << collision << "\n";
+	std::cout << "hit.time: " << hit.time << "\n";
+	std::cout << "hit.front_face: " << hit.front_face << "\n";
+	std::cout << "hit.position.x: " << hit.position.x() << "\n";
+	std::cout << "hit.position.y: " << hit.position.y() << "\n";
+	std::cout << "hit.position.z: " << hit.position.z() << "\n";
+	std::cout << "hit.normal.x: " << hit.normal.x() << "\n";
+	std::cout << "hit.normal.y: " << hit.normal.y() << "\n";
+	std::cout << "hit.normal.z: " << hit.normal.z() << "\n";
+}
+
+void run_sphere_tests()
+{
+	float t_min = 0.0f;
+	float t_max = 10.0f;
+	float sphere_radius = 1.0f;
+	vec3 origin = vec3(0.0f, 0.0f, 0.0f);
+	vec3 positive_x_position = vec3(2 * sphere_radius, 0.0f, 0.0f);
+	vec3 to_negative_x = vec3(-1.0f, 0.0f, 0.0f);
+	ray ray_to_origin = ray(positive_x_position, to_negative_x);
+	sphere sphere_at_origin = sphere(origin, sphere_radius);
+
+	bool collision;
+	hit_record output;
+
+	// center-line hit along x-axis to sphere at origin
+	output = {};
+	collision = sphere_at_origin.hit(ray_to_origin, t_min, t_max, output);
+	std::cout << "\ncenter-line hit along x-axis to sphere at origin\n";
+	print_hit_result(output, collision);
+	assert(collision == true);
+	assert(output.front_face == true);
+	assert(output.time == 1.0f);
+	assert(output.position.x() == 1.0f);
+	assert(output.position.y() == 0.0f);
+	assert(output.position.z() == 0.0f);
+	assert(output.normal.x() == 1.0f);
+	assert(output.normal.y() == 0.0f);
+	assert(output.normal.z() == 0.0f);
+
+	// off center hit parallel to x-axis to sphere at origin
+	vec3 off_center_x_position = vec3(2* sphere_radius, sphere_radius * 0.5f, 0.0f);
+	ray ray_to_off_center_x = ray(off_center_x_position, to_negative_x);
+	output = {};
+	collision = sphere_at_origin.hit(ray_to_off_center_x, t_min, t_max, output);
+	std::cout << "\noff center hit parallel to x-axis to sphere at origin\n";
+	print_hit_result(output, collision);
+	assert(collision == true);
+	assert(output.front_face == true);
+	assert(output.time > 1.0f);
+	assert(output.position.x() < 1.0f);
+	assert(output.position.y() == 0.5f);
+	assert(output.position.z() == 0.0f);
+	assert(output.normal.x() < 1.0f);
+	assert(output.normal.y() == 0.5f);
+	assert(output.normal.z() == 0.0f);
+
+	// off center hit along x-axis to sphere off the origin
+	sphere sphere_off_origin = sphere(vec3(0.0f, -0.5f, 0.0f), sphere_radius);
+
+	output = {};
+	collision = sphere_off_origin.hit(ray_to_origin, t_min, t_max, output);
+	std::cout << "\noff center hit along x-axis to sphere off the origin\n";
+	print_hit_result(output, collision);
+	assert(collision == true);
+	assert(output.front_face == true);
+	assert(output.time > 1.0f);
+	assert(output.position.x() < 1.0f);	// collision point is different to the test case hitting the sphere at the origin
+	assert(output.position.y() == 0.0f);
+	assert(output.position.z() == 0.0f);
+	assert(output.normal.x() < 1.0f);	// normal at collision point is the same as the test case hitting the sphere at the origin
+	assert(output.normal.y() == 0.5f);
+	assert(output.normal.z() == 0.0f);
+}
---
+++ code/vec3_tests.cpp
@@ -0,0 +1,76 @@
+#include "vec3.h"
+#include <assert.h>
+
+void run_vec3_tests()
+{
+	// vec3.length()
+	vec3 unit_vector_x = vec3(1, 0, 0);
+	float x_length = unit_vector_x.length();
+	assert(x_length == 1);
+
+	vec3 unit_vector_y = vec3(0, 1, 0);
+	float y_length = unit_vector_y.length();
+	assert(y_length == 1);
+
+	vec3 unit_vector_z = vec3(0, 0, 1);
+	float z_length = unit_vector_z.length();
+	assert(z_length == 1);
+
+	// unit_vector
+	vec3 x_unit_vector = unit_vector(vec3(45, 0, 0));
+	assert(x_unit_vector.x() == 1);
+	assert(x_unit_vector.y() == 0);
+	assert(x_unit_vector.z() == 0);
+
+	// multiply_by_scalar
+	vec3 scalar_multiple = multiply_by_scalar(vec3(2, 4, 6), 8);
+	assert(scalar_multiple.x() == 16);
+	assert(scalar_multiple.y() == 32);
+	assert(scalar_multiple.z() == 48);
+
+	// add_vectors
+	vec3 vector_addition = add_vectors(vec3(1.2, -2.3, 3.4), vec3(1.2, -2.3, 3.4));
+	assert(vector_addition.x() == (float)2.4);
+	assert(vector_addition.y() == (float)-4.6);
+	assert(vector_addition.z() == (float)6.8);
+
+	vec3 three_vector_addition = add_vectors(vec3(1, -2, 3), vec3(1, -2, 3), vec3(1, -2, 3));
+	assert(three_vector_addition.x() == (float)3);
+	assert(three_vector_addition.y() == (float)-6);
+	assert(three_vector_addition.z() == (float)9);
+
+	// subtract_vectors
+	vec3 vector_subtraction = subtract_vectors(vec3(1.2, -2.3, 3.4), vec3(-1.2, 2.3, -3.4));
+	assert(vector_subtraction.x() == (float)2.4);
+	assert(vector_subtraction.y() == (float)-4.6);
+	assert(vector_subtraction.z() == (float)6.8);
+
+	// dot_product
+	float dot = dot_product(vec3(1, 2, 3), vec3(4, 5, 6));
+	assert(dot == (float)32);
+
+	// random_vec3
+	for (int i = 0; i < 10; ++i)
+	{
+		vec3 random_vector = random_vec3(-45, 45);
+		assert(random_vector.x() < 45);
+		assert(random_vector.x() > -45);
+		assert(random_vector.y() < 45);
+		assert(random_vector.y() > -45);
+		assert(random_vector.z() < 45);
+		assert(random_vector.z() > -45);
+	}
+
+	// random_in_unit_sphere
+	for (int i = 0; i < 10; ++i)
+	{
+		vec3 random_vector_in_sphere = random_in_unit_sphere();
+		assert(random_vector_in_sphere.x() < 1);
+		assert(random_vector_in_sphere.x() > -1);
+		assert(random_vector_in_sphere.y() < 1);
+		assert(random_vector_in_sphere.y() > -1);
+		assert(random_vector_in_sphere.z() < 1);
+		assert(random_vector_in_sphere.z() > -1);
+		assert(random_vector_in_sphere.length_squared() < 1);
+	}
+}
